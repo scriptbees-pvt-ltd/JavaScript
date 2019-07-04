f05d630dbac3468dc5f3850bf1a171b059025a8d
yashu26397 <49430763+yashu26397@users.noreply.github.com>
2019-07-04 12:44:12 +0530

    Add files via upload

---
+++ AddTwoNumbers.js
@@ -0,0 +1,7 @@
+function Sum()
+{
+    var number1 = Number(document.getElementById('number1').value);
+    var number2 = Number(document.getElementById('number2').value);
+    sum = number1+number2;
+    document.getElementById('sum').value=sum;
+}
